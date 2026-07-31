eea2584e91191047d95ec3390011e8eb5d24e109
surajc-15 <surajchanaveeragoudra@gmail.com>
2026-07-31 22:56:56 +0530

    day 6 befoer start verification step all running

--- backend/src/main/java/com/dbtraining/reconx/repository/AuditLogRepository.java
+++ backend/src/main/java/com/dbtraining/reconx/repository/AuditLogRepository.java
@@ -2,6 +2,7 @@ package com.dbtraining.reconx.repository;
 
 import com.dbtraining.reconx.repository.entity.AuditLogEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
